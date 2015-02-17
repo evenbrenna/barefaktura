9fa3db9753dd7cdec97f4cdeb289bff988031069
Even Brenna <evenbrenna@gmail.com>
2015-02-17 18:55:40 +0100

    Refactoring and updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,56 +1,39 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception..
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 
-  # User authentication
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+    # Prevent CSRF attacks by raising an exception..
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 
-  def authenticate_active_admin_user!
+    # Check permitted parameters if controller uses Devise (User)
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # Active Admin uses this to check if user can access back end
+    def authenticate_active_admin_user!
         authenticate_user!
-        unless current_user.role?(:administrator)
+        unless current_user.role? :administrator
             flash[:alert] = "Du har ikke tilgang til denne siden!"
             redirect_to root_path
         end
-  end
+    end
 
-  def after_sign_in_path_for(resource)
-    @user = current_user
-  end
+    # Needed for redirect to user profile on login.
+    def after_sign_in_path_for(resource)
+        @user = current_user
+    end
 
-  def configure_permitted_parameters
-         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email,
-                                                                  :password,
-                                                                  :password_confirmation,
-                                                                  :current_password,
-                                                                  :name,
-                                                                  :address,
-                                                                  :phone,
-                                                                  :org_number,
-                                                                  :bank_name,
-                                                                  :bank_account,
-                                                                  :bank_swift,
-                                                                  :bank_iban,
-                                                                  :ref,
-                                                                  :mva_reg,
-                                                                  :foretaks_reg,
-                                                                  :next_invoice_number)}
+    private
+        # Strong parameters for sign up and account update via Devise
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email,
+                :password, :password_confirmation, :current_password, :name,
+                :address, :phone, :org_number, :bank_name, :bank_account,
+                :bank_swift, :bank_iban, :ref, :mva_reg, :foretaks_reg,
+                :next_invoice_number)}
 
-         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email,
-                                                                        :password,
-                                                                        :password_confirmation,
-                                                                        :current_password,
-                                                                        :name,
-                                                                        :address,
-                                                                        :phone,
-                                                                        :org_number,
-                                                                        :bank_name,
-                                                                        :bank_account,
-                                                                        :bank_swift,
-                                                                        :bank_iban,
-                                                                        :ref,
-                                                                        :mva_reg,
-                                                                        :foretaks_reg,
-                                                                        :next_invoice_number)}
-  end
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email,
+                :password, :password_confirmation, :current_password, :name,
+                :address, :phone, :org_number, :bank_name, :bank_account,
+                :bank_swift, :bank_iban, :ref, :mva_reg, :foretaks_reg,
+                :next_invoice_number)}
+        end
 end
