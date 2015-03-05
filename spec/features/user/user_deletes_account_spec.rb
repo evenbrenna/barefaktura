087a58c0b226900114b253ee9eef6942987f65ca
Even Brenna <evenbrenna@gmail.com>
2015-03-05 23:17:43 +0100

    Fix test

--- spec/features/user/user_deletes_account_spec.rb
+++ spec/features/user/user_deletes_account_spec.rb
@@ -10,19 +10,12 @@ feature 'user deletes account' do
     login_as @user, :scope => :user
   end
 
-  scenario 'without providing password (should delete)', :js => true do
-    page.driver.allow_url('ajax.googleapis.com')
-    page.driver.allow_url('fonts.googleapis.com')
-
+  scenario 'without providing password (should delete)' do
     visit invoices_path
     expect(page).to have_content('Logg Ut')
-
     click_link 'Min Profil'
     click_link 'Rediger Profil'
     click_link 'Slett Konto'
-    expect(page).to have_content('Advarsel')
-    click_link 'Ok'
-
     expect(page).to have_content('Opprett konto')
   end
 end
